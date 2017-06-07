fca4161b182f2d809c567c6b8f4a7de729afec92
jayalakshmiM <jayal3393@gmail.com>
2017-06-07 17:42:43 +0530

    Create mathpower.java

---
+++ mathpower.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class mathpower
+{
+public static void main(String args[])
+{
+
+Scanner in=new Scanner(System.in);
+int n=new int();
+n=(int)(Math.pow(n,2));
+System.out.println(n);
+}
+}
